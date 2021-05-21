1e7092f10b9e1a1cf59a8bc4fabcf28df329ac4b
Michael Hadley <michaelwesthadley@gmail.com>
2021-05-21 16:55:17 -0500

    Switch to single webpack config file with isDev flag

---
+++ config/webpack.config.js
@@ -0,0 +1,44 @@
+const path = require("path");
+const HTMLWebpackPlugin = require("html-webpack-plugin");
+const CopyPlugin = require("copy-webpack-plugin");
+const webpack = require("webpack");
+const babelConfig = require("./.babelrc.js");
+
+module.exports = function (env, argv) {
+  const isDev = argv.mode === "development";
+
+  return {
+    context: path.resolve(__dirname, "..", "src"),
+    entry: "./js/index.ts",
+    module: {
+      rules: [
+        {
+          test: /\.(ts|tsx)$/,
+          use: [{ loader: "babel-loader", options: babelConfig }, "ts-loader"],
+          exclude: /node_modules/,
+        },
+        {
+          test: /\.(js|jsx)$/,
+          exclude: /node_modules/,
+          use: {
+            loader: "babel-loader",
+          },
+        },
+      ],
+    },
+    resolve: {
+      extensions: [".tsx", ".ts", ".js"],
+    },
+    plugins: [
+      new HTMLWebpackPlugin({ template: "./index.html" }),
+      new CopyPlugin({
+        patterns: [{ from: "assets", to: "assets" }],
+      }),
+      new webpack.DefinePlugin({
+        CANVAS_RENDERER: JSON.stringify(true),
+        WEBGL_RENDERER: JSON.stringify(true),
+      }),
+    ],
+    devtool: isDev ? "eval-source-map" : "source-map",
+  };
+};
