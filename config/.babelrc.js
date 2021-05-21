728f314a8690d1e55cc4c00681d2307a367a337b
Michael Hadley <michaelwesthadley@gmail.com>
2021-05-21 16:55:29 -0500

    Simplify babelrc

--- config/.babelrc.js
+++ config/.babelrc.js
@@ -1,13 +1,3 @@
 module.exports = {
-  presets: [
-    [
-      "@babel/preset-env",
-      {
-        targets: "> 0.5%, last 2 versions, Firefox ESR, not dead",
-        useBuiltIns: "entry",
-        corejs: "3.6", // As per core.js docs, use the specific minor version.
-      },
-    ],
-  ],
-  plugins: ["@babel/plugin-transform-runtime"],
+  presets: ["@babel/preset-env"],
 };
